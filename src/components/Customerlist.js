d9c536319601ef080cfb45e1f9e5f810fb114376
komukal <komukal@hotmail.com>
2021-04-13 12:08:28 +0300

    -Added delete functionality to the Customerlist and Trainingslist

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -1,91 +1,107 @@
 import React, { useState, useEffect } from "react";
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
-import {AgGridReact } from 'ag-grid-react';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+import { AgGridReact } from "ag-grid-react";
 
-import { Input, Typography } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from '@material-ui/icons/Edit';
+import { IconButton, Input, Typography } from "@material-ui/core";
 
-function Customerlist(){
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [gridApi,setGridApi]=useState(null);
-    const [columnApi,setColumnApi]=useState(null);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-    const [customers,setCustomers]=useState([]);
-    
-    const fetchCustomers= () => {
-        fetch('http://customerrest.herokuapp.com/api/customers')
-        .then(response => response.json())
-        .then(data => setCustomers(data.content))
-        .catch(err => console.log(err))
+function Customerlist() {
+  const [gridApi, setGridApi] = useState(null);
+  //const [columnApi, setColumnApi] = useState(null);
 
+  const [customers, setCustomers] = useState([]);
+
+  const fetchCustomers = () => {
+    fetch("http://customerrest.herokuapp.com/api/customers")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data.content))
+      .catch((err) => console.log(err));
+  };
+  const deleteCustomer = (url) => {
+    if (window.confirm("Do you want to delete this customer")) {
+      fetch(url, { method: "DELETE" })
+        .then((response) => {
+          if (response.ok) fetchCustomers();
+          else alert("Something went wrong");
+        })
+        .catch((err) => console.error(err));
     }
+  };
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
+  const troubleShooter = (params) => {
+    console.log(params);
+  };
+  const gridColumns = [
+    { field: "firstname", sortable: true, filter: true },
+    { field: "lastname", sortable: true, filter: true },
+    { field: "email", sortable: true, filter: true },
+    {
+      headerName: "Actions",
 
-    useEffect(()=>{
-        fetchCustomers();
-        
-    },[]);
-    const gridColumns=[
-        {field: 'firstname',sortable:true, filter:true},
-        {field: 'lastname',sortable:true, filter:true},
-        {field: 'email',sortable:true, filter:true},
-        { 
-            headerName: '',
-            
-            field: '_links.self.href',
-            cellRendererFramework: params =>
-            <Button variant="contained" color="primary" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-            Manage
-            </Button>
+      field: "links",
+      cellRendererFramework: (params) => (
+        <div>
           
-          
-        }
-    ];
-    function onGridReady(params){
-      setGridApi(params.api);
-      setColumnApi(params.columnApi);
-    }
 
-    const filterText = (e)=>{
-      console.log(e.target.value)
-      gridApi.setQuickFilter(e.target.value);
-    }
-    return(
-      <div border={1}>
-        
-        <div border={1}  className="ag-theme-material"  style={{height:600, width:'60%',margin:'auto'}}>
-          <Typography variant="h5">Customers</Typography>
-          <Input  onChange={filterText} placeholder="Search customers"></Input>
-            <AgGridReact
-            onGridReady={onGridReady}
-            rowData={customers}
-            columnDefs={gridColumns}
-            pagination={true}
-            paginationAutoPageSize={true}
-            suppressCellSelection={true}
-          
-            />
-            <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
+          <IconButton
+            onClick={() => troubleShooter(params.value[0].href)}
+            aria-label="edit"
+            color="primary"
+            size="small"
+          >
+            <EditIcon />
+          </IconButton>
+
+          <IconButton
+            onClick={() => deleteCustomer(params.value[0].href)}
+            aria-label="delete"
+            color="secondary"
+            size="small"
+          >
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      ),
+    },
+    { field: "streetaddress", sortable: true, filter: true },
+    { field: "postcode", sortable: true, filter: true },
+    { field: "phone", sortable: true, filter: true },
+
+
+  ];
+  function onGridReady(params) {
+    setGridApi(params.api);
+    //setColumnApi(params.columnApi);
+  }
+
+  const filterText = (e) => {
+    console.log(e.target.value);
+    gridApi.setQuickFilter(e.target.value);
+  };
+  return (
+    <div border={1}>
+      <div
+        border={1}
+        className="ag-theme-material"
+        style={{ height: 600, width: "60%", margin: "auto" }}
       >
-        <MenuItem onClick={handleClose}>Delete</MenuItem>
-        <MenuItem onClick={handleClose}>Edit info</MenuItem>
-      </Menu>
-        </div>
-        </div>
-    )
+        <Typography variant="h5">Customers</Typography>
+        <Input onChange={filterText} placeholder="Search customers"></Input>
+        <AgGridReact
+          onGridReady={onGridReady}
+          rowData={customers}
+          columnDefs={gridColumns}
+          pagination={true}
+          paginationAutoPageSize={true}
+          suppressCellSelection={true}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Customerlist;
--- src/components/Trainingslist.js
+++ src/components/Trainingslist.js
@@ -1,110 +1,117 @@
 import React, { useState, useEffect } from "react";
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
-import {AgGridReact } from 'ag-grid-react';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { Input, Typography } from "@material-ui/core";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+import { AgGridReact } from "ag-grid-react";
 import moment from "moment";
 
-function Trainingslist(){
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [gridApi,setGridApi]=useState(null);
-    const [columnApi,setColumnApi]=useState(null);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-    const [trainings,setTrainings]=useState([]);
-    
-    const fetchTrainings= () => {
-        fetch('https://customerrest.herokuapp.com/gettrainings')
-        .then(response => response.json())
-        .then(data => setTrainings(data))
-        .catch(err => console.log(err))
 
+import DeleteIcon from "@material-ui/icons/Delete";
+import { IconButton, Input, Typography } from "@material-ui/core";
+
+function Trainingslist() {
+  const [gridApi, setGridApi] = useState(null);
+  //const [columnApi, setColumnApi] = useState(null);
+
+
+  const [trainings, setTrainings] = useState([]);
+
+  const fetchTrainings = () => {
+    fetch("https://customerrest.herokuapp.com/gettrainings")
+      .then((response) => response.json())
+      .then((data) => setTrainings(data))
+      .catch((err) => console.log(err));
+  };
+  const deleteTraining = (id) => {
+    if (window.confirm("Do you want to delete the training")) {
+      const url = 'https://customerrest.herokuapp.com/api/trainings/'+id
+      fetch(url, { method: "DELETE" })
+        .then((response) => {
+          if (response.ok) fetchTrainings();
+          else alert("Something went wrong");
+        })
+        .catch((err) => console.error(err));
     }
+  };
+  useEffect(() => {
+    fetchTrainings();
+  }, []);
+ /*
+  const troubleShooter = (params) => {
+    console.log(params);
+  };
+  */
+  const gridColumns = [
+    { field: "activity", sortable: true, filter: true },
+    {
+      headerName: "Date",
+      field: "date",
+      cellRendererFramework: (params) =>
+        moment(params.value).format("DD.MM.YYYY HH:mm"),
+    },
+    {
+      field: "duration",
+      headerName: "Duration (min)",
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: "Customer",
+      field: "customer",
 
-    useEffect(()=>{
-        fetchTrainings();
-        
-    },[]);
-    const actionButton=(params)=>{
-        console.log(params);
-    }
-    const gridColumns=[
+      cellRendererFramework: (params) => (
+        <div>
+          {params.value.firstname} {params.value.lastname}
+        </div>
+      ),
+    },
 
-        {field: 'activity',sortable:true, filter:true},
-        {
-          headerName:'idk',
-          field: 'date',
-          cellRendererFramework: params =>
-         
-              moment(params.value).format('DD.MM.YYYY HH:mm')
-            
-          
-        }, 
-        {field: 'duration',headerName:'Duration (min)',sortable:true, filter:true},
-        {
-          headerName: 'Customer', field: 'customer',
+    {
+      headerName: "",
 
-          cellRendererFramework: params =>
-           <div>{params.value.firstname} {params.value.lastname}</div>
-        },
-
-        { 
-            headerName: '',
-            
-            field: '_links.self.href',
-            cellRendererFramework: params =>
-            <Button variant="contained" color="primary" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-            Manage
-            </Button>
-          
-          
-        }
-    ];
-    function onGridReady(params){
-      setGridApi(params.api);
-      setColumnApi(params.columnApi);
-    }
-
-    const filterText = (e)=>{
-      console.log(e.target.value)
-      gridApi.setQuickFilter(e.target.value);
-    }
-    return(
-      <div border={1}>
-        
-        <div border={1}  className="ag-theme-material"  style={{height:600, width:'60%',margin:'auto'}}>
-          <Typography variant="h5">Trainings</Typography>
-          <Input  onChange={filterText} placeholder="Search trainings"></Input>
-            <AgGridReact
-            onGridReady={onGridReady}
-            rowData={trainings}
-            columnDefs={gridColumns}
-            pagination={true}
-            paginationAutoPageSize={true}
-            suppressCellSelection={true}
-          
-            />
-            <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
+      field: "_links.self.href",
+      cellRendererFramework: (params) => (
+        <IconButton
+        onClick={() => deleteTraining(params.data.id)}
+        aria-label="delete"
+        color="secondary"
+        size="small"
       >
-        <MenuItem onClick={handleClose}>Delete</MenuItem>
-        <MenuItem onClick={handleClose}>Edit info</MenuItem>
-      </Menu>
-        </div>
-        </div>
-    )
+        <DeleteIcon />
+      </IconButton>
+      ),
+    },
+  ];
+  function onGridReady(params) {
+    setGridApi(params.api);
+    //setColumnApi(params.columnApi);
+  }
+
+  const filterText = (e) => {
+    console.log(e.target.value);
+    gridApi.setQuickFilter(e.target.value);
+  };
+  return (
+    <div border={1}>
+      <div
+        border={1}
+        className="ag-theme-material"
+        style={{ height: 600, width: "60%", margin: "auto" }}
+      >
+        <Typography variant="h5">Trainings</Typography>
+        <Input onChange={filterText} placeholder="Search trainings"></Input>
+        <AgGridReact
+          onGridReady={onGridReady}
+          rowData={trainings}
+          columnDefs={gridColumns}
+          pagination={true}
+          paginationAutoPageSize={true}
+          suppressCellSelection={true}
+        />
+        
+
+      </div>
+    </div>
+  );
 }
 
 export default Trainingslist;
