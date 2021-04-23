8f520608ad1cfd12c7b4890615c233ae610a0855
komukal <komukal@hotmail.com>
2021-04-23 14:48:05 +0300

    -Switched fetch request to HTTPS

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -31,7 +31,7 @@ function Customerlist() {
   const [customers, setCustomers] = useState([]);
 
   const fetchCustomers = () => {
-    fetch("http://customerrest.herokuapp.com/api/customers")
+    fetch("https://customerrest.herokuapp.com/api/customers")
       .then((response) => response.json())
       .then((data) => setCustomers(data.content))
       .catch((err) => console.log(err));
